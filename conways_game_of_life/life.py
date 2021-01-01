49b1727b647092f106ec4a681ee9d23ea86c851f
Eric C. Chen <ericchaichen@gmail.com>
2021-01-01 14:46:56 -0500

    complete life.py

--- conways_game_of_life/life.py
+++ conways_game_of_life/life.py
@@ -1,3 +1,7 @@
+from pprint import pprint
+from time import sleep
+from copy import deepcopy
+from pdb import set_trace
 def simulate(rows, cols, steps, live_cells):
     """Run Conway's Game of Life on a finite (rows x cols) grid.
 
@@ -7,17 +11,53 @@ def simulate(rows, cols, steps, live_cells):
     sequence of (row, column) pairs. Prints the grid after each
     time step.
     """
-    ...
 
-    
-# The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, live or dead, (or populated and unpopulated, respectively). Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:
+    # Initialize live_cells grid
+    grid = [[0 for i in range(cols + 2)] for j in range(rows + 2)]
+    for r, c in live_cells:
+        grid[r + 1][c + 1] = 1
 
-# Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-# Any live cell with two or three live neighbours lives on to the next generation.
-# Any live cell with more than three live neighbours dies, as if by overpopulation.
-# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-# These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
+    print_grid(grid)
 
-# Any live cell with two or three live neighbours survives.
-# Any dead cell with three live neighbours becomes a live cell.
-# All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+    # Iterate over steps
+    for _ in range(steps):
+        grid2 = deepcopy(grid)
+        for r in range(1, rows + 1):
+            for c in range(1, cols + 1):
+                # Count Live Neighbors
+                live_neighbors = 0
+                relative_neighbor_idx = [(0,1), (0,-1), (1,0), (-1,0), (1,1), (1,-1), (-1,1), (-1,-1)]
+                for n1, n2 in relative_neighbor_idx:
+                    if grid[r + n1][c + n2]:
+                        live_neighbors += 1
+
+                # Any live cell with two or three live neighbours survives. All other live cells die in the next generation. 
+                if grid[r][c] and live_neighbors != 2 and live_neighbors != 3:
+                    grid2[r][c] = 0
+                
+                # Any dead cell with three live neighbours becomes a live cell. All other dead cells stay dead.
+                if not grid[r][c] and live_neighbors == 3:
+                    grid2[r][c] = 1
+
+        # Print grid
+        print_grid(grid2)
+        grid = grid2
+        sleep(0.2)
+
+def print_grid(grid):
+    rows, cols = len(grid), len(grid[0])
+    print('-' * cols)
+
+    for r in range(rows):
+        if r == 0 or r == rows - 1: continue
+
+        print('|', end='')
+        for c in range(cols):
+            if c == 0 or c == cols - 1: continue
+
+            if grid[r][c]:
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print('|')
+    print('-' * cols)
